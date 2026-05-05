74b56e63e6d6de93f797e142b7757c6ae32c7e81
AB <amitvishnu@gmail.com>
2026-05-05 20:26:19 +0530

    Update Microsoft Clarity Tracking ID

--- analytics.js
+++ analytics.js
@@ -5,6 +5,6 @@
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "wjpct66h02");
+})(window, document, "clarity", "script", "wmeizfxlku");
 
 console.log("Analytics: Microsoft Clarity Initialized");
